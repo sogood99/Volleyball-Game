1117701edbe940c25f5f0f26850a9c1d81fb47d6
Vitaly Pronin <Vitaly202@gmail.com>
2021-04-25 05:45:57 -0400

    Improved ball visuals.
    The ball now rotates along with its direction of velocity. It also squashes & stretches based on the magnitude of that velocity.

--- Centril City Balleyball/Assets/Scripts/Ball.cs
+++ Centril City Balleyball/Assets/Scripts/Ball.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public float maxSpd = 1;
+    private Rigidbody2D rb;
+    public float maxSpd = 70;
+
+    public Transform sprite;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Direction reflects angle of velocity vector
+        direction = rb.velocity.normalized;
 
+        // Ball sprite rotation should match direction
+        sprite.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity.normalized);
+
+        // Stretch the ball based on speed
+        if (rb.velocity.magnitude >= maxSpd/2)
+        {
+            float surplusSpd = rb.velocity.magnitude - (maxSpd/2);
+            float stretch = surplusSpd / (maxSpd/2);
+            sprite.localScale = new Vector2(1 - (stretch * 1/3), 1 + stretch);
+        }
+        else
+            sprite.localScale = new Vector2(1, 1);
     }
 
     // Put all of the rigidbody stuff in here
